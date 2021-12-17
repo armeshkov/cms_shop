8128e131b60e04f2c99511f97bc6e51c277c0d1b
axmeshkov <axmeshkov@gmail.com>
2021-12-17 07:42:13 +0300

    displayed popular products on the main page

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -10,9 +10,9 @@ class MainController extends AppController
 
     public function indexAction() {
         $brands = \R::find('brand', 'LIMIT 3');
-        debug($brands);
+        $hits = \R::find('product', "hit = '1' AND status = '1' LIMIT 8");
         $this->setMeta('Главная страница', 'Описание', 'Ключевые слова');
-        $this->set(compact('brands'));
+        $this->set(compact('brands', 'hits'));
     }
 
 }
